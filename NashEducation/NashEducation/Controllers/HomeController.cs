b13240baa5453c4db72642b5a210cdb490f93ae7
Pavel_Aksyutin <thxu2@ya.ru>
2018-03-11 16:59:54 +0300

    тест (reverted from commit 833ce4dfa28474ab6ecea09756436a286f404d81)

--- NashEducation/NashEducation/Controllers/HomeController.cs
+++ NashEducation/NashEducation/Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace NashEducation.Controllers
 
         public ActionResult Contact()
         {
+        //blabla
             ViewBag.Message = "Your contact page.";
 
             return View();
